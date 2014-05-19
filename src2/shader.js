9895e13f4204ca48bc58ab7e69d83b95c32e163b
Mr Speaker <mrspeaker@gmail.com>
2014-05-19 17:15:08 -0600

    lol

--- src2/shader.js
+++ src2/shader.js
@@ -7,23 +7,6 @@ let col = (pos) => {
 export { col }
 
 /*
-var sin = Math.sin,
-	cos = Math.cos,
-	xr = pos.xr,
-	yr = pos.yr,
-	t = pos.t,
-	s = 0;
-
-s += sin(xr * cos(t / 300) * 80) + cos(yr * cos(t / 300) * 10);
-s += sin(yr * sin(t / 200) * 40) + cos(xr * sin(t / 500) * 40);
-s += sin(xr * sin(t / 100) * 10) + sin(yr * sin(t / 600) * 80);
-s *= sin(t / 200) * 0.5;
-
-return [s, s * 0.5, sin(s + t / 30) * 0.75];
-
-
-
-
 let glsl = ({xr, yr, t}) => {
 	let {sin, cos} = Math,
 		s = 0;
